d1fdd08b38db649d8c7814084a76cd119ea59259
geneticsjesse <40606030+geneticsjesse@users.noreply.github.com>
2023-03-26 19:02:36 -0400

    Adjust collinearity check, plot feature importance

--- featureSelection.py
+++ featureSelection.py
@@ -2,11 +2,13 @@
 # Author:   Jesse Wolf, jwolf@uoguelph.ca | Thomas Papp-Simon, tpappsim@uoguelph.ca
 # Date:     March 18, 2023
 
-# How to run: python3 featureSelection.py -in merged_df_outliers_removed.csv
+# How to run: python3 featureSelection.py -in merged_df_outliers_removed_CFS.csv
 # ================= #
 
 from sklearn.feature_selection import RFE
 from sklearn.linear_model import LogisticRegression
+from sklearn import preprocessing
+from matplotlib import pyplot
 import pandas as pd
 import argparse
 import sys
@@ -29,14 +31,38 @@ df = pd.read_csv(filename)
 
 # Separate input and output variables
 varray = df.values
+
 # ncols = len(varray[0,:])-1
-X = varray[:,13:] # All continuous variables
-Y = varray[:,7] # Win/Loss
+#X = varray[:,12:] # All continuous variables
+#Y = varray[:,7] # Win/Loss
+X=df.values[:,2:]
+Y=df.values[:,0]
+
+# Scale the data to facilitate feature selection
+scaler = preprocessing.StandardScaler().fit(X)
+X_scaled = scaler.transform(X)
 
 # Feature selection
-model = LogisticRegression(solver='lbfgs', max_iter=1)
-rfe = RFE(model, n_features_to_select = 10)
-fit = rfe.fit(X,Y.astype(float))
+model = LogisticRegression(solver='lbfgs', max_iter=1000)
+model.fit(X,Y)
+rfe = RFE(model, n_features_to_select = 5)
+fit = rfe.fit(X,Y)
+
+#Perform permutation importance
+importance = model.coef_[0]
+x_axis = [x for x in range(len(importance))]
+
+pyplot.figure(figsize=(10,7))
+pyplot.bar(x_axis, importance)
+pyplot.xlabel('Features')
+pyplot.ylabel('Importance')
+pyplot.xticks(range(0,17))
+pyplot.savefig(f'featureImportance/feature_Importance.png')
+#pyplot.show()
+
+for i, v in enumerate(importance):
+    print ('Feature: %0d, Score: %.5f' % (i,v))
+
 print("Num features: %d" % fit.n_features_)
 print("Selected features: %s" % fit.support_)
 print("Feature Ranking: %s" % fit.ranking_)
--- multicollinearityCheck.py
+++ multicollinearityCheck.py
@@ -34,20 +34,10 @@ filename = args.in_file
 # load the dataset
 df = pd.read_csv(filename)
 
-# Slice the dataframe to only look at our continuous predictor variables
-df_slice = df.iloc[:, 12:]
-
-# set figure size
-plt.figure(figsize=(18,15))
-
-# Generate a mask to onlyshow the bottom triangle
-mask = np.triu(np.ones_like(df_slice.corr(), dtype=bool))
-
-# generate heatmap
-sns.heatmap(df_slice.corr(), annot=True, mask=mask, vmin=-1, vmax=1)
-plt.title('Correlation Coefficient Of Predictors')
-plt.savefig(f'collinearityPlots/predictorCorrelationMatrix.png')
-#plt.show()
+# Slice the dataframe to only look at our continuous predictor variables and also retain wl_home (our output variable) and game_yearEnd, which we will use to split our train/tesitng data.
+df_slice = df.iloc[:, 7:]
+# Drop some columns we don't need
+df_slice = df_slice.drop(['team_id_away', 'team_abbreviation_away', 'team_name_away'], axis=1)
 
 # compute the vif for all given features
 def compute_vif(considered_features):
@@ -67,34 +57,56 @@ def compute_vif(considered_features):
 considered_features = ['FG.', 'percent_3pt', 'percent_2pt', 'percent_FT', 'DRB', 'ORB', 'TRB', 'AST', 'STL', 'BLK', 'TOV', 'PF', 'PTS', 'MarginOfVictory', 'StrengthOfSchedule', 'SimpleRatngSystem', 'ORtg', 'DRtg', 'NRtg', 'Pace', 'FTr', 'X3PAr', 'TS.']
 
 # compute vif 
-#print (compute_vif(considered_features).sort_values('VIF', ascending=False))
+print (compute_vif(considered_features).sort_values('VIF', ascending=False))
 
 # We see that there are some values >10, so we can remove and re-compute VIF.
 # compute vif values after removing a feature
 considered_features.remove('ORtg')
-#print(compute_vif(considered_features))
+print(compute_vif(considered_features))
 
 # Still, we have some high values of VIF, on we go.
 considered_features.remove('SimpleRatngSystem')
-#print(compute_vif(considered_features))
+print(compute_vif(considered_features))
 
 # Still, we have some high values of VIF, on we go.
 considered_features.remove('MarginOfVictory')
+
+# We can still see some high correlations and VIF values >5, so we will remove TS. as it has a VIF of ~8.3
+considered_features.remove('TS.')
 print(compute_vif(considered_features))
 
-# Let's plot our correlation matrix again to see how it looks after removing ORtg, SimpleRatngSystem, and MarginOfVictory
-
+# Now that all VIF values are <5, we can re-plot our correlation matrix and determine if we are satisfied that we are no longer including correlated variables
 # First, we have to create a new data frame without the three columns we removed above.
-df_slice = df_slice.drop(['ORtg', 'SimpleRatngSystem', 'MarginOfVictory'], axis=1)
+df_slice = df_slice.drop(['ORtg', 'SimpleRatngSystem', 'MarginOfVictory', 'TS.'], axis=1)
 
+# Now, all of our VIF values are <5, but there are still some large correlations, so we will filter out anything with a correlation value >0.5
+#Correlation with output variable
+cor = df_slice.corr()
+cor_target = abs(cor)
+#Selecting highly correlated features
+relevant_features = cor_target[cor_target>0.5]
+print (relevant_features)
+
+# We can see that we have three pairwise comparisons with Pearson correlation values >0.5 (Pace-percent_2pt, X3PAr-percent_2pt, and Pace-X3PAr). We will remove X3PAr and percent_2pt and re-evaluate the correlation matrix.
+df_slice = df_slice.drop(['X3PAr', 'percent_2pt'], axis=1)
+cor = df_slice.corr()
+cor_target = abs(cor)
+#Selecting highly correlated features
+relevant_features = cor_target[cor_target>0.5]
+#print (relevant_features)
+
+# Perfect, we have no more pairwise variables with a pearson correlation coefficient >0.5. In addition, we have no variables with a VIF value over 5. We plot our final correlation matrix and output our new data frame.
 # set figure size
 plt.figure(figsize=(18,15))
 
 # Generate a mask to onlyshow the bottom triangle
-mask = np.triu(np.ones_like(df_slice.corr(), dtype=bool))
+mask = np.triu(np.ones_like(df_slice.iloc[:, 2:].corr(), dtype=bool))
 
 # generate heatmap
-sns.heatmap(df_slice.corr(), annot=True, mask=mask, vmin=-1, vmax=1)
+sns.heatmap(df_slice.iloc[:, 2:].corr(), annot=True, mask=mask, vmin=-1, vmax=1)
 plt.title('Correlation Coefficient Of Predictors')
-plt.savefig(f'collinearityPlots/predictorCorrelationMatrix_reduced.png')
-#plt.show()
+#plt.savefig(f'collinearityPlots/predictorCorrelationMatrix_reduced.png')
+plt.show()
+
+# Now that we have performed correlation-based feature selection, our new data frame is ready for downstream analyses.
+df_slice.to_csv ('merged_df_outliers_removed_CFS.csv', index = False)
