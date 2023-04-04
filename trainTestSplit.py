c0c643d21bb6001580c6417aee6d142bb52956b7
taps99 <tpappsim@uoguelph.ca>
2023-04-04 15:06:59 -0400

    script for splitting training/testing data

---
+++ trainTestSplit.py
@@ -0,0 +1,11 @@
+# ========================================================================= #
+# Training and Test data splitting 
+#
+# Author:   Jesse Wolf, jwolf@uoguelph.ca | Thomas Papp-Simon, tpappsim@uoguelph.ca
+# Date:     April 4, 2023
+#
+# How to run:   python3 trainTestSplit.py -in merged_df_outliers_removed_CFS.csv  -m1 knn  -m2 linear -n 10
+# ========================================================================= #
+
+import pandas as pd
+
