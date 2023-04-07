90496da2cafe29c3cde14f1ee7549100a334a7b5
taps99 <tpappsim@uoguelph.ca>
2023-04-07 16:07:14 -0400

    blah

--- misc_unused/scalingX.py
+++ misc_unused/scalingX.py
@@ -17,21 +17,21 @@ import pandas as pd
 from sklearn import preprocessing
 
 # define command line arguments
-parser = argparse.ArgumentParser(description='Classification Models')
-parser.add_argument('--in_file', '-in', action="store", dest='in_file', required=True, help='Name of csv input file. The last colum of the file is the desired output.')
-#parser.add_argument('--method', '-m', action="store", dest='method', default='linear', required=False, help='Method: adaboost, ridgec, dtc, gbc, knn, linear, linsvc, mlp, rf, sgd, svc')
-#parser.add_argument('--kfold', '-k', action="store", dest='kfold', default=5, required=False, help='Number of folds for cross-validation')
-#parser.add_argument('--num_splits', '-n', action="store", dest='num_splits', default=3, required=False, help='Number of folds for cross-validation')
+# parser = argparse.ArgumentParser(description='Classification Models')
+# parser.add_argument('--in_file', '-in', action="store", dest='in_file', required=True, help='Name of csv input file. The last colum of the file is the desired output.')
+# #parser.add_argument('--method', '-m', action="store", dest='method', default='linear', required=False, help='Method: adaboost, ridgec, dtc, gbc, knn, linear, linsvc, mlp, rf, sgd, svc')
+# #parser.add_argument('--kfold', '-k', action="store", dest='kfold', default=5, required=False, help='Number of folds for cross-validation')
+# #parser.add_argument('--num_splits', '-n', action="store", dest='num_splits', default=3, required=False, help='Number of folds for cross-validation')
 
-# handle user errors
-try:
-    args = parser.parse_args()
-except:
-    parser.print_help()
-    sys.exit(0)
+# # handle user errors
+# try:
+#     args = parser.parse_args()
+# except:
+#     parser.print_help()
+#     sys.exit(0)
 
-# save arguments in separate variables
-filename    = args.in_file
+# # save arguments in separate variables
+# filename    = args.in_file
 # load the dataset
 df = pd.read_csv(filename)
 # Slice off non-numeric columns
@@ -57,5 +57,5 @@ scaled_df = pd.concat([scaled_df.reset_index(drop=True), df_extra], axis=1)
 scaled_df['wl_home'] = Y
 
 print (scaled_df)
-
+scaled_df.to_csv ('merged_df_outliers_removed_Xscaled.csv', index = False)
 #scaled_df.to_csv ('merged_df_outliers_removed_RFE_Xscaled.csv', index = False)
