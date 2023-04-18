b86f5124eeed412da212fe20797bec317e4f7588
geneticsjesse <40606030+geneticsjesse@users.noreply.github.com>
2023-04-18 17:21:05 -0400

    stacking without RF+MLP, added MCCPlot and fixed RFE

--- masterScript_testRun/RFE.py
+++ masterScript_testRun/RFE.py
@@ -48,24 +48,20 @@ for filename in files:
         new_filename = filename_re.group(1)
 
     df_rfe_clean = pd.concat([merged_df[selected_features_rfe].reset_index(drop=True), df_extra], axis=1)
-    df_rfe_clean.to_csv(f'./RFE_splits/RFE_{new_filename}.csv', index = False)
+    #df_rfe_clean.to_csv(f'./RFE_splits/RFE_{new_filename}.csv', index = False)
 
 ### Creating test set with same features as RFE_training2015-2021_outliers_removed_scaled.csv
 RFE_training = pd.read_csv(f"./RFE_splits/RFE_training2015-2021_outliers_removed_scaled.csv")
 scaled_test_set = pd.read_csv(f"./scaled_training_sets/test_set_outliers_removed_scaled.csv")
 
 scaled_test_set = scaled_test_set.drop([col for col in scaled_test_set.columns if col not in scaled_test_set.columns and col not in RFE_training.columns], axis=1)
-scaled_test_set.to_csv(f'./RFE_splits/test_RFE_all.csv')
-
-
-
+#scaled_test_set.to_csv(f'./RFE_splits/test_RFE_all.csv')
 
 # This code should loop over all files in the scaled_training_sets directory and only process the ones with the .csv file extension and ending with RFE.csv. It then loads each data frame from the CSV file and stores it in a dictionary using the file name as the key. Finally, it creates a list of column names for each data frame and prints them to the console.
 directory = "RFE_splits"
 file_extension = "RFE"
 
-# # Create an empty dictionary to store the data frames and their column names
-data_frames = {}
+# # Create an empty list to store each dataframe
 frames = []
 # # Loop over all files in the directory
 for file_name in os.listdir(directory):
@@ -73,36 +69,9 @@ for file_name in os.listdir(directory):
     if file_name.startswith(file_extension):
         # Load the data frame from the CSV file
         df = pd.read_csv(os.path.join(directory, file_name))
-        # Store the data frame in the dictionary using the file name as the key
-        #data_frames[file_name] = df
         frames.append(df)
+# Create a list of the columns(features) that are shared among all datasets.
 common_cols = list(set.intersection(*(set(df.columns) for df in frames)))
-print(common_cols)
-        # print(frames)
-        # Store the column names in a list for each data frame
-        # column_names = list(df.columns)
-        # print(column_names)
-        
-
-#This code creates a set of the column names for the first data frame in the dictionary, then loops over all the other data frames and updates the set of common column names with the intersection of the current data frame's columns and the existing set. Finally, it prints the resulting set of common column names.
-#
-# Create a list of the column names for the first data frame in the dictionary
-# common_columns = list(data_frames[list(data_frames.keys())[0]].columns)
-
-# # Loop over all other data frames in the dictionary
-# for key in list(data_frames.keys())[0:]:
-#     # Create a new list to store the common column names for this data frame
-#     current_common_columns = []
-#     # Loop over the column names in this data frame
-#     for col in data_frames[key].columns:
-#         # Check if this column name is in the list of common column names so far
-#         if col in common_columns:
-#             # If it is, add it to the current list of common column names
-#             current_common_columns.append(col)
-#     # Replace the list of common column names with the current list
-#     common_columns = current_common_columns
-#     print(str(len(common_columns))) 
-
 
 # # Print the common column names, or a message if there are no common column names
 # if len(common_columns) > 0:
@@ -114,8 +83,14 @@ print(common_cols)
 train2015_2021 = pd.read_csv('./RFE_splits/RFE_training2015-2021_outliers_removed_scaled.csv')
 test = pd.read_csv('./scaled_training_sets/test_set_outliers_removed_scaled.csv') 
 
+# Reset both train and test dataset to only contain the RFE_common columns
 train2015_2021 = train2015_2021.loc[:, common_cols]
 test = test.loc[:, common_cols]
 
+# Re-index columns to be in same order as the rest of the datasets
+train2015_2021 = train2015_2021.reindex(columns=['percent_3pt', 'percent_2pt', 'DRB', 'ORB', 'TRB', 'STL', 'DRtg', 'NRtg', 'TS.','team_abbreviation_home', 'team_abbreviation_away', 'game_date', 'game_yearEnd','wl_home'])
+test = test.reindex(columns=['percent_3pt', 'percent_2pt', 'DRB', 'ORB', 'TRB', 'STL', 'DRtg', 'NRtg', 'TS.','team_abbreviation_home', 'team_abbreviation_away', 'game_date', 'game_yearEnd','wl_home'])
+
+# Create new csvs for downstream use
 train2015_2021.to_csv('./RFE_splits/train2015_2021_RFEcommon.csv', index=False)
 test.to_csv('./RFE_splits/test_RFEcommon.csv', index = False)    
